e0d4e73b9760a9a804f1e22bb1fcd066d8abcc26
blawabar <fake.torrent@wp.pl>
2020-03-11 19:39:00 +0100

    modify height of the Header component

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -9,7 +9,6 @@ const Header = () => {
     <header className="main-header">
       <div className="main-header__glass-pane">
         <h1 className="main-header__intro-heading">foodie haven</h1>
-        {/* <p className="main-header__catchline">wide option of choice</p> */}
         <p className="main-header__catchline">
           Delicious recipes and wide option of choice
         </p>
