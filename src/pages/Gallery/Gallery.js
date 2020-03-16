c46456c0872619250d182c2d9d6e5fe30a2444f9
blawabar <fake.torrent@wp.pl>
2020-03-17 00:06:50 +0100

    add css fix for GalleryGrid component; add initial implementation of Gallery page

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -2,8 +2,62 @@ import React from "react";
 
 import "./Gallery.scss";
 
+import PageHeading from "../../components/PageHeading";
+import asparagus from "../../images/jause_640.jpg";
+
 const Gallery = () => {
-  return <h1>Hello, I am the Gallery Page</h1>;
+  return (
+    <section className="gallery">
+      <PageHeading large>Food and Restaurant Gallery</PageHeading>
+      <section className="gallery__masonry-grid">
+        <div className="image-frame">
+          <img src={asparagus} alt="asparagus_640.jpg" />
+        </div>
+        <div className="image-frame">
+          <img src={asparagus} alt="asparagus_640.jpg" />
+        </div>
+        <div className="image-frame">
+          <img src={asparagus} alt="asparagus_640.jpg" />
+        </div>
+        <div className="image-frame">
+          <img src={asparagus} alt="asparagus_640.jpg" />
+        </div>
+        <div className="image-frame">
+          <img src={asparagus} alt="asparagus_640.jpg" />
+        </div>
+        <div className="image-frame">
+          <img src={asparagus} alt="asparagus_640.jpg" />
+        </div>
+        <div className="image-frame">
+          <img src={asparagus} alt="asparagus_640.jpg" />
+        </div>
+        <div className="image-frame">
+          <img src={asparagus} alt="asparagus_640.jpg" />
+        </div>
+        <div className="image-frame">
+          <img src={asparagus} alt="asparagus_640.jpg" />
+        </div>
+        <div className="image-frame">
+          <img src={asparagus} alt="asparagus_640.jpg" />
+        </div>
+        <div className="image-frame">
+          <img src={asparagus} alt="asparagus_640.jpg" />
+        </div>
+        <div className="image-frame">
+          <img src={asparagus} alt="asparagus_640.jpg" />
+        </div>
+        <div className="image-frame">
+          <img src={asparagus} alt="asparagus_640.jpg" />
+        </div>
+        <div className="image-frame">
+          <img src={asparagus} alt="asparagus_640.jpg" />
+        </div>
+        <div className="image-frame">
+          <img src={asparagus} alt="asparagus_640.jpg" />
+        </div>
+      </section>
+    </section>
+  );
 };
 
 export default Gallery;
