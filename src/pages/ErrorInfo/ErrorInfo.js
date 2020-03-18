8409b99a886b9411d9dbdab1c98229f0b1161c9f
blawabar <fake.torrent@wp.pl>
2020-03-18 19:56:49 +0100

    add implementation for ErrorInfo page

--- src/pages/ErrorInfo/ErrorInfo.js
+++ src/pages/ErrorInfo/ErrorInfo.js
@@ -2,8 +2,20 @@ import React from "react";
 
 import "./ErrorInfo.scss";
 
+import { Link } from "react-router-dom";
+
 const ErrorInfo = () => {
-  return <h1>Hello, I am the ErrorInfo Page</h1>;
+  return (
+    <section className="error-info">
+      <div className="error-info__card">
+        <h1 className="error-info__code">404</h1>
+        <h2 className="error-info__message">page not found</h2>
+        <Link to="/" className="error-info__link-btn">
+          back to the home page
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default ErrorInfo;
