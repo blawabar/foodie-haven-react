5525a13b4370ec5bbc0375444b327d4a384d51d9
blawabar <fake.torrent@wp.pl>
2020-03-19 21:36:18 +0100

    add missing commit -  update Gallery page with Animator and MasonryGrid component

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -3,21 +3,19 @@ import React from "react";
 import "./Gallery.scss";
 
 import PageHeading from "../../components/PageHeading";
-import asparagus from "../../images/jause_640.jpg";
+import MasonryGrid from "../../components/MasonryGrid";
+
+import Animator from "../../components/Animator";
 
 const Gallery = () => {
   return (
     <section className="gallery">
-      <PageHeading large>Food and Restaurant Gallery</PageHeading>
-      <section className="gallery__masonry-grid">
-        {Array.from({ length: 16 }, item => asparagus).map((imgSrc, indx) => {
-          return (
-            <div className="image-frame" key={indx}>
-              <img src={imgSrc} alt="mock-data" />
-            </div>
-          );
-        })}
-      </section>
+      <Animator>
+        <PageHeading large>Food and Restaurant Gallery</PageHeading>
+      </Animator>
+      <Animator>
+        <MasonryGrid></MasonryGrid>
+      </Animator>
     </section>
   );
 };
