8f3f1640cae679ffa7dddca6b4f6aafcd3fb99d2
blawabar <fake.torrent@wp.pl>
2020-03-20 18:38:15 +0100

    move images data from MasonryGrid to the Gallery page

--- src/components/MasonryGrid/MasonryGrid.js
+++ src/components/MasonryGrid/MasonryGrid.js
@@ -2,28 +2,6 @@ import React from "react";
 
 import "./MasonryGrid.scss";
 
-// This is a temporary solution
-// I'm going to use some automated mechanism for fetching img data statically
-
-const masonryImages = [
-  "kitchen_640.jpg",
-  "asparagus_640.jpg",
-  "cook_640.jpg",
-  "breakfast_640.jpg",
-  "table_640.jpg",
-  "jause_640.jpg",
-  "birthday_640.jpg",
-  "catering_640.jpg",
-  "kitchen_640.jpg",
-  "asparagus_640.jpg",
-  "cook_640.jpg",
-  "breakfast_640.jpg",
-  "table_640.jpg",
-  "jause_640.jpg",
-  "birthday_640.jpg",
-  "catering_640.jpg"
-];
-
 const ImageCell = ({ imgSrc }) => {
   return (
     <div className="image-cell">
@@ -35,10 +13,10 @@ const ImageCell = ({ imgSrc }) => {
   );
 };
 
-const MasonryGrid = () => {
+const MasonryGrid = ({ images }) => {
   return (
     <section className="masonry-grid">
-      {masonryImages.map((imgSrc, indx) => {
+      {images.map((imgSrc, indx) => {
         return <ImageCell key={indx} imgSrc={imgSrc} />;
       })}
     </section>
--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -7,6 +7,25 @@ import MasonryGrid from "../../components/MasonryGrid";
 
 import Animator from "../../components/Animator";
 
+const images = [
+  "kitchen_640.jpg",
+  "asparagus_640.jpg",
+  "cook_640.jpg",
+  "breakfast_640.jpg",
+  "table_640.jpg",
+  "jause_640.jpg",
+  "birthday_640.jpg",
+  "catering_640.jpg",
+  "kitchen_640.jpg",
+  "asparagus_640.jpg",
+  "cook_640.jpg",
+  "breakfast_640.jpg",
+  "table_640.jpg",
+  "jause_640.jpg",
+  "birthday_640.jpg",
+  "catering_640.jpg"
+];
+
 const Gallery = () => {
   return (
     <section className="gallery">
@@ -14,7 +33,7 @@ const Gallery = () => {
         <PageHeading large>Food and Restaurant Gallery</PageHeading>
       </Animator>
       <Animator>
-        <MasonryGrid></MasonryGrid>
+        <MasonryGrid images={images}></MasonryGrid>
       </Animator>
     </section>
   );
