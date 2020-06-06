ddaf733281b66a3381fe243079e99f0f66995b74
blawabar <fake.torrent@wp.pl>
2020-06-06 16:59:26 +0200

    Updates

---
+++ precache-manifest.f3f5146a1bc668a76e82090846321a63.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a72bca500c4fae95aee83c90047d338e",
+    "url": "/foodie-haven-react/index.html"
+  },
+  {
+    "revision": "7b33a3641a7d6dddb150",
+    "url": "/foodie-haven-react/static/css/0.c7fa5217.chunk.css"
+  },
+  {
+    "revision": "33a115be272ff77b278e",
+    "url": "/foodie-haven-react/static/css/10.d0457339.chunk.css"
+  },
+  {
+    "revision": "2d54f2aa6d91864c93a3",
+    "url": "/foodie-haven-react/static/css/11.ce48008e.chunk.css"
+  },
+  {
+    "revision": "7556e0e7e4434830744b",
+    "url": "/foodie-haven-react/static/css/4.af564fa3.chunk.css"
+  },
+  {
+    "revision": "03ec3495acdda9e15833",
+    "url": "/foodie-haven-react/static/css/5.3532a5a6.chunk.css"
+  },
+  {
+    "revision": "ac3b904372301638eb02",
+    "url": "/foodie-haven-react/static/css/6.61cd9c42.chunk.css"
+  },
+  {
+    "revision": "2ca1607a23692953235b",
+    "url": "/foodie-haven-react/static/css/8.dca32b69.chunk.css"
+  },
+  {
+    "revision": "fcdbfb36fb5b16e2bcee",
+    "url": "/foodie-haven-react/static/css/9.988c1682.chunk.css"
+  },
+  {
+    "revision": "1a0a85b8f0c48573f023",
+    "url": "/foodie-haven-react/static/css/main.8a651601.chunk.css"
+  },
+  {
+    "revision": "7b33a3641a7d6dddb150",
+    "url": "/foodie-haven-react/static/js/0.a03631f0.chunk.js"
+  },
+  {
+    "revision": "33a115be272ff77b278e",
+    "url": "/foodie-haven-react/static/js/10.eb58dacb.chunk.js"
+  },
+  {
+    "revision": "2d54f2aa6d91864c93a3",
+    "url": "/foodie-haven-react/static/js/11.70f3c12c.chunk.js"
+  },
+  {
+    "revision": "114813a93d1f0488e21f",
+    "url": "/foodie-haven-react/static/js/3.db3ee397.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/foodie-haven-react/static/js/3.db3ee397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7556e0e7e4434830744b",
+    "url": "/foodie-haven-react/static/js/4.38d8199b.chunk.js"
+  },
+  {
+    "revision": "03ec3495acdda9e15833",
+    "url": "/foodie-haven-react/static/js/5.5e1d10d0.chunk.js"
+  },
+  {
+    "revision": "ac3b904372301638eb02",
+    "url": "/foodie-haven-react/static/js/6.db0aec74.chunk.js"
+  },
+  {
+    "revision": "09caae53e7d1e0503649",
+    "url": "/foodie-haven-react/static/js/7.74de02f1.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/foodie-haven-react/static/js/7.74de02f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ca1607a23692953235b",
+    "url": "/foodie-haven-react/static/js/8.95502beb.chunk.js"
+  },
+  {
+    "revision": "fcdbfb36fb5b16e2bcee",
+    "url": "/foodie-haven-react/static/js/9.f456a612.chunk.js"
+  },
+  {
+    "revision": "1a0a85b8f0c48573f023",
+    "url": "/foodie-haven-react/static/js/main.d4f8673d.chunk.js"
+  },
+  {
+    "revision": "e090d147731f1ea7d2bb",
+    "url": "/foodie-haven-react/static/js/runtime-main.c51368c3.js"
+  },
+  {
+    "revision": "ff3c8f3263835abea73982e443893c09",
+    "url": "/foodie-haven-react/static/media/asparagus_640.ff3c8f32.jpg"
+  },
+  {
+    "revision": "e4332371b8c9df8a1f3c52b8fbf153b3",
+    "url": "/foodie-haven-react/static/media/birthday_1280.e4332371.jpg"
+  },
+  {
+    "revision": "9ff4e82d5ea3e6d707d7ddaa3d551172",
+    "url": "/foodie-haven-react/static/media/birthday_640.9ff4e82d.jpg"
+  },
+  {
+    "revision": "e678c7d55a2e58510f97b4e28235ce8d",
+    "url": "/foodie-haven-react/static/media/breakfast_640.e678c7d5.jpg"
+  },
+  {
+    "revision": "7b092313f780bdd87d9a4d8287502a02",
+    "url": "/foodie-haven-react/static/media/catering_1280.7b092313.jpg"
+  },
+  {
+    "revision": "f195c0b574dd0ab6036f9eaf75cb0f7b",
+    "url": "/foodie-haven-react/static/media/catering_640.f195c0b5.jpg"
+  },
+  {
+    "revision": "22285385f9888305c5dbf888dd85893b",
+    "url": "/foodie-haven-react/static/media/chef_1280.22285385.jpg"
+  },
+  {
+    "revision": "8f3972dbebfe3e401285ea4f9e13c225",
+    "url": "/foodie-haven-react/static/media/cook_640.8f3972db.jpg"
+  },
+  {
+    "revision": "ff25e3c9630b7193585dfb16273da5fe",
+    "url": "/foodie-haven-react/static/media/jause_1280.ff25e3c9.jpg"
+  },
+  {
+    "revision": "106cf3143a092ad78374da85ed5420e4",
+    "url": "/foodie-haven-react/static/media/jause_640.106cf314.jpg"
+  },
+  {
+    "revision": "24e611fa81ae6ced68c5bac8451c8f11",
+    "url": "/foodie-haven-react/static/media/kitchen_640.24e611fa.jpg"
+  },
+  {
+    "revision": "cd67055ba5172a91114c7376eb665492",
+    "url": "/foodie-haven-react/static/media/restaurant_1280.cd67055b.jpg"
+  },
+  {
+    "revision": "fd4cd5c377240aa34e0a647e3d90bc70",
+    "url": "/foodie-haven-react/static/media/restaurant_640.fd4cd5c3.jpg"
+  },
+  {
+    "revision": "0f27d4f915428da51e77ad7629db0ed6",
+    "url": "/foodie-haven-react/static/media/salmon_1280.0f27d4f9.jpg"
+  },
+  {
+    "revision": "e49edfe558b6bade49d0181913682963",
+    "url": "/foodie-haven-react/static/media/service_640.e49edfe5.jpg"
+  },
+  {
+    "revision": "49a2d49f10ccc91b7390eb09ea6fe6fb",
+    "url": "/foodie-haven-react/static/media/serving_640.49a2d49f.jpg"
+  },
+  {
+    "revision": "0140d683ada565caf9a5c81e2c6a2ad2",
+    "url": "/foodie-haven-react/static/media/table_640.0140d683.jpg"
+  },
+  {
+    "revision": "9cdfe4a4149bfad4248b18cc3d5714b2",
+    "url": "/foodie-haven-react/static/media/telephone_1280.9cdfe4a4.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/foodie-haven-react/precache-manifest.f3f5146a1bc668a76e82090846321a63.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/foodie-haven-react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
